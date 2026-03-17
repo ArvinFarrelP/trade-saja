24353ef22822f8389d4e43e264f583cc1f239b0e
ArvinFarrelP <farel877629@gmail.com>
2026-03-17 07:04:10 +0700

    build success

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap'; ❌ disable
 
 export default defineConfig({
-  site: 'https://tradesaja.vercel.app',
-  // site: 'https://arvinfarrel.github.io/trade-saja',
+  site: 'https://trade-saja.vercel.app/',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     mdx(),
-    sitemap(),
+    // sitemap(), ❌ disable
   ],
   markdown: {
     shikiConfig: {
